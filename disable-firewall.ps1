33feeba1f62453b6195172b1f0831a12268f2a6f
johnnyedev <156373071+johnnyedev@users.noreply.github.com>
2025-01-28 22:06:28 -0600

    disable-firewall.ps1

---
+++ disable-firewall.ps1
@@ -0,0 +1,3 @@
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile' -name "EnableFirewall" -Value 0
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile' -name "EnableFirewall" -Value 0
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\Standardprofile' -name "EnableFirewall" -Value 0
